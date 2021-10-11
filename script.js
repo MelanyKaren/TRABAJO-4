5de9f5dabbbef7d5998cefdaa6da00a2fe6fdda0
MelanyKaren <melany10vg@gmail.com>
2021-10-10 22:17:07 -0500

    primero

---
+++ script.js
@@ -0,0 +1,10 @@
+let canción = document.getElementById("canción");
+let play_btn = document.getElementById("play-btn");
+let pause_btn = document.getElementById("pause-btn");
+
+play_btn.addEventListener("click", ()=>{
+    canción.play();
+});
+pause_btn.addEventListener("click", ()=>{
+    canción.play();
+});
